ec80d0d881436610b1c2b51d16bfe0de064f9ad8
ludovicjj <jahanlud@gmail.com>
2019-04-29 09:37:34 +0200

    Add field ChoiceType in CreateUserType & create CreateUserDTO

---
+++ src/AppBundle/DTO/CreateUserDTO.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AppBundle\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class CreateUserDTO
+{
+    /**
+     * @var string|null
+     * @Assert\NotBlank(message="Vous devez saisir un nom d'utilisateur.")
+     * @Assert\Length(
+     *     max=25,
+     *     maxMessage="Votre nom d'utilisateur ne peut pas excéder 25 caractéres."
+     * )
+     */
+    public $username;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank(message="Vous devez saisir un mot de passe.")
+     * @Assert\Length(
+     *     max=64,
+     *     maxMessage="Votre mot de passe ne peut pas excéder 64 caractéres."
+     * )
+     */
+    public $password;
+
+    /**
+     * @var string|null
+     * @Assert\NotBlank(message="Vous devez saisir une adresse email.")
+     * @Assert\Email(message="Le format de l'adresse n'est pas correcte.")
+     * @Assert\Length(
+     *     max=60,
+     *     maxMessage="Votre email ne peut pas excéder 60 caractéres."
+     * )
+     */
+    public $email;
+
+    /**
+     * @var bool|null
+     */
+    public $roles;
+
+    /**
+     * AddUserInput constructor.
+     * @param string|null $username
+     * @param string|null $password
+     * @param string|null $email
+     * @param bool|null $roles
+     */
+    public function __construct(
+        string $username = null,
+        string $password = null,
+        string $email = null,
+        bool $roles = null
+    ) {
+        $this->username = $username;
+        $this->password = $password;
+        $this->email = $email;
+        $this->roles = $roles;
+    }
+}
--- src/AppBundle/Form/CreateUserType.php
+++ src/AppBundle/Form/CreateUserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class CreateUserType extends AbstractType
 {
@@ -23,6 +24,14 @@ class CreateUserType extends AbstractType
                 'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
             ])
             ->add('email', EmailType::class, ['label' => 'Adresse email'])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => false,
+                    'Administrateur' => true,
+                ],
+                'label' => "Rôle",
+                'required' => true,
+            ])
         ;
     }
 }
