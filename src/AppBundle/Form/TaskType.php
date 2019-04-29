ff8e702ae7bbafaeedfd3315a5fff702ea8cb00d
ludovicjj <jahanlud@gmail.com>
2019-04-29 11:09:10 +0200

    Mapping form TaskType to TaskDTO

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -2,10 +2,13 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\DTO\TaskDTO;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormInterface;
 
 class TaskType extends AbstractType
 {
@@ -15,5 +18,18 @@ class TaskType extends AbstractType
             ->add('title', TextType::class, ['label' => 'titre'])
             ->add('content', TextareaType::class, ['label' => 'Contenu'])
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => TaskDTO::class,
+            'empty_data' => function (FormInterface $form) {
+                return new TaskDTO(
+                    $form->get('title')->getData(),
+                    $form->get('content')->getData()
+                );
+            }
+        ]);
     }
 }
