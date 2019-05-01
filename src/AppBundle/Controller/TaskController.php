44a2a4a440191c6e63cdb98c4edbf781bc23e0c3
ludovicjj <jahanlud@gmail.com>
2019-05-01 14:41:56 +0200

    Move methode createAction in Controller/task/AddTaskController. Update methode with AddTaskHandler. Return Response or RedirectResponse

--- src/AppBundle/Controller/TaskController.php
+++ src/AppBundle/Controller/TaskController.php
@@ -19,30 +19,6 @@ class TaskController extends Controller
     }
 
     /**
-     * @Route("/tasks/create", name="task_create")
-     */
-    public function createAction(Request $request)
-    {
-        $task = new Task();
-        $form = $this->createForm(TaskType::class, $task);
-
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-
-            $em->persist($task);
-            $em->flush();
-
-            $this->addFlash('success', 'La tâche a été bien été ajoutée.');
-
-            return $this->redirectToRoute('task_list');
-        }
-
-        return $this->render('task/create.html.twig', ['form' => $form->createView()]);
-    }
-
-    /**
      * @Route("/tasks/{id}/edit", name="task_edit")
      */
     public function editAction(Task $task, Request $request)
