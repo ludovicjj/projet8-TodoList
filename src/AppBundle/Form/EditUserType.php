8efe9c983de4702c260a9d126bd8c7cac1d48da5
ludovicjj <jahanlud@gmail.com>
2019-05-08 11:51:26 +0200

    EditUserForm getParent CreateUserType and extends AbstractType

--- src/AppBundle/Form/EditUserType.php
+++ src/AppBundle/Form/EditUserType.php
@@ -3,14 +3,20 @@
 namespace AppBundle\Form;
 
 use AppBundle\DTO\EditUserDTO;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EditUserType extends CreateUserType
+class EditUserType extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
             'data_class' => EditUserDTO::class,
         ]);
+    }
+
+    public function getParent()
+    {
+        return CreateUserType::class;
     }
 }
